602ba0c98607efec95c3dcae285af0f5848ed899
wangzhitao <wangzhitao@huajiao.tv>
2018-04-28 18:35:20 +0800

    init project

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python36
+# -*- coding: UTF-8 -*-
+
+import keyword
+
+
+print("Hello Python!");
+print("======================");
+print(keyword.kwlist);
+a = 200;
+if a >= 0:
+    print(a);
+else:
+    print(-a);
